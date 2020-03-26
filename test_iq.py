b30d90cee4cd1117cfd89f5d7d9645ac4d056c21
C. E. Sharp <chrisesharp@gmail.com>
2020-03-26 18:21:37 +0000

    constant -> statement(s)

--- test_iq.py
+++ test_iq.py
@@ -1,8 +1,10 @@
 import unittest
 
 def iq_test(string_of_numbers):
-    return 3
+    numbers = [int(x) for x in string_of_numbers.split()]
+    odd_position = numbers.index(7) + 1    
+    return odd_position
 
 class IQTest(unittest.TestCase):
     def test_odd_one_out(self):
-        self.assertEqual(2, iq_test("2 4 7 8 10"))
+        self.assertEqual(3, iq_test("2 4 7 8 10"))
