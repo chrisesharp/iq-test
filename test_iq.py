77177d891340a4be93b0fb036ee135bf89f1ddbe
C. E. Sharp <chrisesharp@gmail.com>
2020-03-26 18:24:48 +0000

    new odd number test

--- test_iq.py
+++ test_iq.py
@@ -1,10 +1,13 @@
 import unittest
 
 def iq_test(string_of_numbers):
-    numbers = [int(x) for x in string_of_numbers.split()]
-    odd_position = numbers.index(7) + 1    
+    odd_numbers = [int(x)%2!=0 for x in string_of_numbers.split()]
+    odd_position = odd_numbers.index(True) + 1
     return odd_position
 
 class IQTest(unittest.TestCase):
-    def test_odd_one_out(self):
+    def test_7_as_odd_one_out(self):
         self.assertEqual(3, iq_test("2 4 7 8 10"))
+    
+    def test_3_as_odd_one_out(self):
+        self.assertEqual(2, iq_test("2 3 4 8 10"))
