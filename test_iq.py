acbcf689e83a2e72c2b2373aeaf46ed4ac96eb10
C. E. Sharp <chrisesharp@gmail.com>
2020-03-26 18:27:29 +0000

    test last element

--- test_iq.py
+++ test_iq.py
@@ -12,7 +12,10 @@ class IQTest(unittest.TestCase):
     def test_3_as_odd_one_out(self):
         self.assertEqual(2, iq_test("2 3 4 8 10"))
     
-    def test_1_as_odd_one_out(self):
+    def test_first_as_odd_one_out(self):
         self.assertEqual(1, iq_test("1 2 2"))
     
+    def test_last_as_odd_one_out(self):
+        self.assertEqual(3, iq_test("2 4 5"))
     
+
