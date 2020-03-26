7897b1381ceb17365630af3c04e4014a1328ddb3
C. E. Sharp <chrisesharp@gmail.com>
2020-03-26 18:14:12 +0000

    make fail

--- test_iq.py
+++ test_iq.py
@@ -1,8 +1,8 @@
 import unittest
 
-def iq_test(input):
+def iq_test(string_of_numbers):
     return 3
 
 class IQTest(unittest.TestCase):
     def test_odd_one_out(self):
-        self.assertEqual(3, iq_test("2 4 7 8 10"))
+        self.assertEqual(2, iq_test("2 4 7 8 10"))
